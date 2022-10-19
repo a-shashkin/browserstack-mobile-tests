f118f6e681a5ca599505c9009fb6a9f95f0cdea6
a-shashkin <caezar1993@gmail.com>
2022-10-19 20:25:02 +0400

    Intermediate push: added JUnit5 annotations.

--- src/test/java/tests/BrowserstackAndroidSecondTest.java
+++ src/test/java/tests/BrowserstackAndroidSecondTest.java
@@ -3,16 +3,23 @@ package tests;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.net.MalformedURLException;
-
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
+@Feature("Главная страница")
+@Owner("Alexander Shashkin")
 public class BrowserstackAndroidSecondTest extends TestBase {
     @Test
-    void secondTest() throws MalformedURLException, InterruptedException {
+    @Tag("selenide_android")
+    @Story("Проверка наличия элементов на главном экране")
+    @DisplayName("Проверка наличия заголовков In the news и Featured article на главном экране")
+    void secondTest() {
         $$(MobileBy.id("org.wikipedia.alpha:id/view_card_header_title"))
                 .filterBy(Condition.text("In the news")).shouldHave(CollectionCondition.sizeGreaterThan(0));
         $$(MobileBy.id("org.wikipedia.alpha:id/view_card_header_title"))
